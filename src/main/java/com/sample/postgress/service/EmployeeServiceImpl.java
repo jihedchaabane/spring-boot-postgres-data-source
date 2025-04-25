1cf86711cd74237e067bbc95d5e8a126f5d4a8d7
JihedGitlab <chaaben.jihed.gitlab@gmail.com>
2025-04-25 14:50:10 +0200

    ..

---
+++ src/main/java/com/sample/postgress/service/EmployeeServiceImpl.java
@@ -0,0 +1,41 @@
+package com.sample.postgress.service;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.sample.postgress.dao.EmployeeDao;
+import com.sample.postgress.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+	@Resource
+	private EmployeeDao employeeDao;
+
+	@Override
+	public List<Employee> findAll() {
+		return employeeDao.findAll();
+	}
+
+	@Override
+	public void insertEmployee(Employee emp) {
+		employeeDao.insertEmployee(emp);
+	}
+
+	@Override
+	public void updateEmployee(Employee emp) {
+		employeeDao.updateEmployee(emp);
+	}
+
+	@Override
+	public void executeUpdateEmployee(Employee emp) {
+		employeeDao.executeUpdateEmployee(emp);
+	}
+
+	@Override
+	public void deleteEmployee(Employee emp) {
+		employeeDao.deleteEmployee(emp);
+	}
+}
